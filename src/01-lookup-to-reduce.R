66a0ca66f77ead19c1df9d0b60b40974b4540b24
lucienbaumgartner <luciengeorges.baumgartner@uzh.ch>
2021-03-03 08:34:50 +0100

    tidy filtering

--- src/01-lookup-to-reduce.R
+++ src/01-lookup-to-reduce.R
@@ -1,24 +1,62 @@
+## ---------------------------
+##
+## Script name: 00b-sweep.R
+##
+## Project: Tracing Thick Concepts
+##
+## Purpose of script: Collect data from raw API responses (local)
+##
+## Author: Lucien Baumgartner
+##
+## Date created: 12.11.2021
+##
+## Email: lucienbaumgartner@philos.uzh.ch
+##
+## ---------------------------
+##
+## Notes:
+##    DO NOT RUN
+##
+## ---------------------------
+
+## ---------------------------
+######## 1 Libraries #########
+## ---------------------------
 library(dplyr)
 library(stringr)
 library(spacyr)
 library(gtools)
 library(tokenizers)
 library(pbmcapply)
-
 rm(list=ls())
-setwd('~/tc_methods_paper/src')
 
+## ---------------------------
+## 2 Set working directory ###
+## ---------------------------
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+#setwd('~/tc_methods_paper/src')
+getwd()
+
+## ---------------------------
+######## 3 Load data #########
+## ---------------------------
 datasets <- list.files('/Volumes/INTENSO/methods_paper/output/00-bulk-data/baseline/reddit/raw_aggr', full.names = T, pattern = '\\%22')
 search.terms <- read.table('../input/dict_rerun_02_09_20.txt', header = T, stringsAsFactors = F, sep=',')
 
+## ---------------------------
+####### 4 Collect Data #######
+## ---------------------------
 for(i in datasets){
-  
   #i = datasets[1]
   load(i)
   df <- tibble(txt=dta)
-  .lookup <- paste0(paste0('(\\w+(\\,)?\\s\\b(and|but)\\b\\s\\b', search.terms$word, '\\b', ')|(\\b', search.terms$word, '\\b'), '(\\,)?\\s\\b(and|but)\\b\\s\\w+)', collapse = '|')
+  ## Formulate regex
+  .lookup <- paste0(paste0('(\\w+(\\,)?\\s\\b(and|but)\\b\\s\\b', 
+                           search.terms$word, '\\b', ')|(\\b', 
+                           search.terms$word, '\\b'), 
+                    '(\\,)?\\s\\b(and|but)\\b\\s\\w+)', collapse = '|')
+  ## Extract regex matches
   corpus <- pbmclapply(df$txt, function(x){
-    #print(x)
     tmp <- tokenizers::tokenize_sentences(x)
     tmp <- unlist(tmp)
     tmp <- tolower(tmp)
@@ -26,17 +64,17 @@ for(i in datasets){
     tmp <- unname(tmp)
     return(tmp)
   }, mc.cores = 4)
-  
+  ## Reduce initial response according to the occurence of target structures (nrow X n matches)
   df <- cbind(df[rep(1:nrow(df), lengths(corpus)),], corpus=unlist(corpus)) %>% as_tibble
   rm(corpus)
   df <- mutate(df, corpus = as.character(corpus))
+  ## Extract matches and add them to df
   reg_matches <- pbmclapply(df$corpus, function(x) str_extract_all(x, .lookup), mc.cores=4)
   reg_matches <- unlist(reg_matches, recursive=F)
   df <- cbind(df[rep(1:nrow(df), lengths(reg_matches)),], match=unlist(reg_matches)) %>% as_tibble
   df <- mutate(df, match = as.character(match))
   df <- as_tibble(df)
-  # save data
+  ## Save data
   out <- paste0('/Volumes/INTENSO/methods_paper/output/01-reduced-corpora/baseline/reddit/new-', gsub('.*\\/', '', i))
   save(df, file = out, compress = 'gzip')
-  
 }
