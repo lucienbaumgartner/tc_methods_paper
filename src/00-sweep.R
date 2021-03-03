81f6b262770c3e5d4d9b34d4cf108bb38ca35bc9
lucienbaumgartner <luciengeorges.baumgartner@uzh.ch>
2021-03-03 08:25:15 +0100

    tidy API-sweep

--- src/00-sweep.R
+++ src/00-sweep.R
@@ -1,4 +1,29 @@
 # !diagnostics off
+
+## ---------------------------
+##
+## Script name: 00b-sweep.R
+##
+## Project: Tracing Thick Concepts
+##
+## Purpose of script: Handler for API calls
+##
+## Author: Lucien Baumgartner
+##
+## Date created: 12.11.2021
+##
+## Email: lucienbaumgartner@philos.uzh.ch
+##
+## ---------------------------
+##
+## Notes:
+##    DO NOT RUN
+##
+## ---------------------------
+
+## ---------------------------
+######## 1 Libraries #########
+## ---------------------------
 library(dplyr)
 library(pbmcapply)
 library(httr)
@@ -13,10 +38,16 @@ library(jsonlite)
 library(utc)
 rm(list=ls())
 
-#setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-setwd('~/tc_methods_paper/src')
+## ---------------------------
+## 2 Set working directory ###
+## ---------------------------
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+#setwd('~/tc_methods_paper/src')
 getwd()
 
+## ---------------------------
+######## 3 Load data #########
+## ---------------------------
 search.terms <- read.table('../input/dict_rerun_02_09_20.txt', header = T, stringsAsFactors = F, sep=',')
 search.terms <- 
   tibble(word=c(paste0('%22', capitalize(search.terms$word), '%20and%22'), 
@@ -24,6 +55,11 @@ search.terms <-
          cat=rep(search.terms$cat, 2),
          cancel=ifelse(grepl('20and%', word), 0, 1))
 
+## ---------------------------
+########### 4 UDFs ###########
+## ---------------------------
+## formulate the regex to extract specific regex matches from responses
+## according to PoS-tags
 syntax.regex <- '(ADV\\s)?ADJ\\s(PUNCT\\s)?CCONJ\\s(ADV\\s)?ADJ'
 make_regex <- function(INDEX){
   TARGET = paste0('\\\\b',df$TARGET[INDEX], '\\\\b')
@@ -71,6 +107,9 @@ make_regex <- function(INDEX){
   if(is.list(tmp)|is.null(tmp)) return(tmp)
 }
 
+## ---------------------------
+##### 4 Formulate Calls ######
+## ---------------------------
 Lurls <- lapply(search.terms$word, function(x){
   paste0(
     'https://api.pushshift.io/reddit/search/comment/?q=',
@@ -80,24 +119,33 @@ Lurls <- lapply(search.terms$word, function(x){
 
 names(Lurls) <- search.terms$word
 
-#-c(1:61)
-for(m in search.terms$word[-c(1:70)]){
+## ---------------------------
+######## 5 Do Calls ##########
+## ---------------------------
+## Starting from t1, we make calls for every URL
+## until we have 5000 usable observations.
+## Each call uses the datetime of the last call 
+## as a starting point. If the calls do not
+## produce usable observations, we induce
+## a time jump of a day (480000 utc units).
+for(m in search.terms$word){
   #m <- search.terms$word[1]
-  # extract root of API call
-  urls <- Lurls[[m]]
+  urls <- Lurls[[m]] # extract root of API call
   print(m)
-  # reset parameters used in the loop
+  ## ---------------------------
+  ## Reset parameters used in the loop
   rowLOG <- T # maintains the outer while loop
   its <- 0 # iteration index
-  # time index
-  time.index <- toUTC(as.Date('31-08-2020', '%d-%m-%Y')) 
+  time.index <- toUTC(as.Date('31-08-2020', '%d-%m-%Y')) # time index
   time.index <- as.numeric(time.index)
-  # index to decide whether to induce time jump
-  BREAK.COUNTER.OLD <- 0
-  BREAK.COUNTER.NEW <- 0
+  BREAK.COUNTER.OLD <- 0  # index to decide whether to induce time jump
+  BREAK.COUNTER.NEW <- 0  # indices to decide whether to induce time jump
   
+  ## ---------------------------
+  ## Make calls until 5000 observations are collected
   while(rowLOG){
-    # complete API request
+    ## ---------------------------
+    ## Complete API request
     if(its == 0){geturl <- paste0(urls, time.index)}else{
       if(BREAK.COUNTER.OLD < BREAK.COUNTER.NEW){
         time.index <- time.index - 480000
@@ -112,18 +160,18 @@ for(m in search.terms$word[-c(1:70)]){
       geturl <- paste0(urls, time.index)
     }
     
-    # actual API call
+    ## ---------------------------
+    ## Actual API call
     TRYING <- T
     while(TRYING){
       response <- try(jsonlite::fromJSON(geturl))
       TRYING <- 'try-error' %in% class(response)
     }
-    # coerce to tibble
+    ## Coerce to tibble
     df <- as_tibble(response$data)
-    
-    # if the API call is empty, skip to next url
+    ## If the API call is empty, skip to next url
     if(nrow(df)==0) break
-    # test whether all the variables we need are present
+    ## Test whether all the variables we need are present
     if(!'body'%in%names(df)){
       df <- mutate(df, body = NA)
     }else if(!'id'%in%names(df)){
@@ -131,16 +179,17 @@ for(m in search.terms$word[-c(1:70)]){
     }else if(!'created_utc'%in%names(df)){
       df <- mutate(df, created_utc = NA)
     }
-    # mutate df
+    ## Mutate df
     df <- rename(df, txt = body)
     df <- select(df, id, txt, created_utc)
     
-    # regex
-    .word <- tolower(stri_extract(regex='(?<=22)(.*)(?=\\%20)',str=urls))
+    ## ---------------------------
+    ## Text mining to filter out the target structures BY LEXICAL CONTENT
+    .word <- tolower(stri_extract(regex='(?<=22)(.*)(?=\\%20)',str=urls)) # regex
     .lookup <- paste0(paste0('(\\w+(\\,)?\\s\\b(and|but)\\b\\s\\b', .word, '\\b', ')|(\\b', .word, '\\b'), '(\\,)?\\s\\b(and|but)\\b\\s\\w+)', collapse = '|')
-    # text mining to filter out the target structures
-     corpus <- mclapply(unlist(df$txt), function(x){
-      #print(x)
+    
+    ## Mining Operation
+    corpus <- mclapply(unlist(df$txt), function(x){
       tmp <- tokenizers::tokenize_sentences(x)
       tmp <- unlist(tmp)
       tmp <- tolower(tmp)
@@ -149,33 +198,36 @@ for(m in search.terms$word[-c(1:70)]){
       return(tmp)
     }, mc.cores = 4)
     
-    # reduce initial response according to the occurence of target structures
+    ## Reduce initial response according to the occurence of target structures (nrow X n matches)
     df <- cbind(df[rep(1:nrow(df), lengths(corpus)),], corpus=unlist(corpus)) %>% as_tibble
     rm(corpus)
     df <- mutate(df, corpus = as.character(corpus))
-    # extract matches and add it to df
+    
+    ## Extract matches and add tehm to df
     reg_matches <- mclapply(df$corpus, function(x) stringr::str_extract_all(x, .lookup), mc.cores=4)
     reg_matches <- unlist(reg_matches, recursive=F)
     df <- cbind(df[rep(1:nrow(df), lengths(reg_matches)),], match=unlist(reg_matches)) %>% as_tibble
     df <- mutate(df, match = as.character(match))
     df <- as_tibble(df)
     
-    # mutate df (mainly unlisting match metadata)
+    ## Mutate df (mainly unlisting match metadata)
     df <- mutate(df, TARGET = strsplit(gsub('\\,', '', match), '\\s'))
     df <- mutate(df, CCONJ = sapply(TARGET, function(x) return(x[x %in% c('but')][1])))
     df <- mutate(df, TARGET = sapply(df$TARGET, function(x) return(x[x %in% .word][1])))
     df <- mutate(df, comma = grepl('\\,', match))
     
-    # annotate text
+    ## ---------------------------
+    ## PoS-tag filtering
+    ## Annotate text with PoS-tags
     txtparsed <- spacy_parse(tolower(df$corpus), pos = TRUE)
     txtparsed <- split(txtparsed, txtparsed$doc_id, lex.order = F)
     txtparsed <- txtparsed[gtools::mixedsort(names(txtparsed))]
     txtparsed <- mclapply(txtparsed, function(x) x$lemma %>% setNames(., x$pos), mc.cores = 4)
-    
-    # inspect target structures and filter out the false positives
+
+    ## Inspect target structures and filter out the false positives
     txtparsed_adj <- mclapply(1:length(txtparsed), make_regex, mc.cores=4)
     
-    # expand df to accomodate the finalized matching
+    ## Expand df to accomodate the finalized matching
     reps <- unlist(lapply(sapply(txtparsed_adj, nrow), function(x) ifelse(is.null(x), 0, x)))
     df <- df[rep(1:nrow(df), reps),]
     txtparsed_adj <- do.call(rbind, txtparsed_adj)
@@ -183,36 +235,33 @@ for(m in search.terms$word[-c(1:70)]){
     df <- cbind(df, txtparsed_adj)
     df <- as_tibble(df)
     
-    # last quecks
+    ## Last checks
     df <- filter(df, TARGET%in%.word) # only targets
     df <- df[,!sapply(df[,1:length(df)], is.list)] # filter out list variables
     
-    # saving process and meta-updates
+    ## ---------------------------
+    ## Saving process and meta-updates
     out <- paste0('../output/00-sweep/', m, '.csv')
-    # if file doesn't exist, make it
+    ## If file doesn't exist, make it
     if(!file.exists(out)){
       write.csv(df, file=out, row.names = F)
       rowLOG <- T
     }else{
-      # if df has no rows after the matching, it means that the current 100 API responses do not contain target structures and we have to initiate time jump
+      ## If df has no rows after the matching, it means that the current 100 API responses do not contain target structures and we have to initiate time jump
       if(nrow(df) == 0){
         BREAK.COUNTER.NEW <- BREAK.COUNTER.OLD + 1
         if(BREAK.COUNTER.NEW - BREAK.COUNTER.OLD > 1) BREAK.COUNTER.OLD <- (BREAK.COUNTER.NEW - 1)
       }
-      # append data
+      ## Append data
       df2 <- read.csv(out) %>% as_tibble
       df <- rbind(df2, df)
       df <- filter(df, !duplicated(id))
       write.csv(df, file=out, row.names = F)
       print(nrow(df))
-      # update the log for the while loop
+      ## Update the log for the while loop
       rowLOG <- !nrow(df) >= 5000
     }
-    # udate iteration index
+    ## Udate iteration index
     its <- its + 1
   }
 }
-
-
-
-
